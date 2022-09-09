98885feb96836e744f3629bbe52e70d78f947792
Quan Nguyen <nquan1007@gmail.com>
2022-09-09 15:54:22 +0700

    #1 Commonly used syntax to find an item in an array

--- JavaScript-Examples/index.js
+++ JavaScript-Examples/index.js
@@ -1,7 +1,17 @@
+// 1. find() method
 const array1 = [5, 12, 8, 130, 44];
 
 const found = array1.find(element => element > 10);
 
 console.log(found);
 
-// return first item that returns true
+// 2. Commonly used syntax
+const inventory = [
+  {name: 'apples', quantity: 2},
+  {name: 'bananas', quantity: 0},
+  {name: 'cherries', quantity: 5}
+];
+
+const found1 = inventory.find(inventory => inventory.name === 'bananas');
+
+console.log(found1);
