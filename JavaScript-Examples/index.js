870ae5edf2feb1757d7faec608fd07f0fe036c34
Quan Nguyen <nquan1007@gmail.com>
2022-09-12 16:40:58 +0700

    #1 Example about why we should use callback

--- JavaScript-Examples/index.js
+++ JavaScript-Examples/index.js
@@ -1,17 +1,27 @@
-// Inheritance
+// The callback function is executed asynchronously
+console.log("before");
+setTimeout(() => {
+  // runs after 2 seconds
+  console.log("inside the function");
+}, 2000);
+console.log("after");
 
-class Greet {
-  sayHi() {
-    return "How are you?";
-  }
+// Example about callback
+var users = ["Quan", "An", "Tuan"];
+
+function addUser(username, callback) {
+  setTimeout(function () {
+    users.push(username);
+    callback(); // printUser is called only after a new username is pushed into the array
+  }, 200);
 }
 
-class Answer extends Greet {
-  sayHi() {
-    return super.sayHi() + " I'm fine, thank you!"
-  }
+function printUser() {
+  setTimeout(function() {
+    console.log(users);
+  }, 100);
 }
 
-const howToAnswer = new Answer();
+addUser('Tai', printUser);
 
-console.log(howToAnswer.sayHi());
+printUser();
