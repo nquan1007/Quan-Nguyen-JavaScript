9b68747fea45706d8dbf6ca5954b073e6804ff58
Quan Nguyen <nquan1007@gmail.com>
2022-09-12 10:26:21 +0700

    #1 Learn about Object Methods

--- JavaScript-Examples/index.js
+++ JavaScript-Examples/index.js
@@ -1,8 +1,32 @@
-// 1. Return multiple values
-function getData() {
-  return ["Flavio", 37];
-}
-let [username, age] = getData();
-console.log([username, age]);
+const car = {
+  brand: "Ford",
+  model: "Fiesta",
+  start: function () {
+    console.log(`Started
+  ${this.brand} ${this.model}`);
+  },
+};
+car.start();
+delete car.model;
+console.log(car.model);
 
+// we don't have access to `this` if we use an arrow function
+const car2 = {
+  brand: "Ford",
+  model: "Fiesta",
+  start: () => {
+    console.log(`Started
+  ${this.brand} ${this.model}`);
+  },
+};
+car2.start();
 
+// Methods can accept parameters, like regular functions
+const car3 = {
+  brand: "Ford",
+  model: "Fiesta",
+  goTo: function (destination) {
+    console.log(`Going to ${destination}`);
+  },
+};
+car3.goTo("Rome");
