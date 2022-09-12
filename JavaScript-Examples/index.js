8f406b9e947a86def5e1197b1c0931fd51d7aaeb
Quan Nguyen <nquan1007@gmail.com>
2022-09-12 08:50:56 +0700

    #1 How loops operate

--- JavaScript-Examples/index.js
+++ JavaScript-Examples/index.js
@@ -1,17 +1,30 @@
-// 1. find() method
-const array1 = [5, 12, 8, 130, 44];
-const found = array1.find(element => element > 10);
-console.log(found);
+// 1. while loops
+const list = ["a", "b", "c"];
+let i = 0;
+while (i < list.length) {
+  console.log(list[i]); //value
+  console.log(i); //index
+  i = i + 1;
+}
 
-// 2. Commonly used syntax
-const inventory = [
-  {name: 'apples', quantity: 2},
-  {name: 'bananas', quantity: 0},
-  {name: 'cherries', quantity: 5}
-];
-const found1 = inventory.find(inventory => inventory.name === 'bananas');
-console.log(found1);
+// 2. do..while loops
+const list2 = ["a2", "b2", "c2"];
+let i2 = 0;
+do {
+  console.log(list2[i2]); //value
+  console.log(i2); //index
+  i2 = i2 + 1;
+} while (i2 < list.length);
 
-// 3. includes() method
-const found2 = array1.includes(130);
-console.log(found2);
+// 3. for loops
+const list3 = ["a3", "b3", "c3"];
+for (let i = 0; i < list3.length; i++) {
+  console.log(list3[i]); //value
+  console.log(i); //index
+}
+
+// 4. for..of loops
+const list4 = ["a4", "b4", "c4"];
+for (const value of list4) {
+  console.log(value); //value
+}
