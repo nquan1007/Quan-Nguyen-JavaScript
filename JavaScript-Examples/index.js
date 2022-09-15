77a75c6f435dd4623f28a627ad52eed17da9c28b
QuanNguyen <nquan1007@gmail.com>
2022-09-15 15:06:44 +0700

    #1 JSON parse and stringify

--- JavaScript-Examples/index.js
+++ JavaScript-Examples/index.js
@@ -6,6 +6,22 @@ const jsonBoolean = 'true';
 
 const jsonNull = 'null';
 
+const jsonString = '"Quan"'
+
 const jsonArray = '["JavaScript","PHP"]';
 
 const jsonObject = '{"name":"Quan Nguyen","age":"23"}';
+
+console.log(JSON.parse(jsonNumber));
+console.log(typeof JSON.parse(jsonNumber));
+
+var object = JSON.parse(jsonObject);
+console.log(object);
+
+var An = {
+  ...object,
+  name: "An Nguyen"
+}
+console.log(An);
+
+console.log(JSON.stringify(1));
