9fbaf8d6928f057bd8d3d4556eeef4fb233817c6
Quan Nguyen <nquan1007@gmail.com>
2022-09-19 08:48:22 +0700

    #1 ES6: Enhanced Objet Property including Property Shorthand, Method Property & Computed Property

--- JavaScript-Examples/index.js
+++ JavaScript-Examples/index.js
@@ -1,9 +1,46 @@
-// ES6 Template Literals 
-const age = 23;
-console.log(`Nam nay toi ${age} tuoi
-Toi ten la Quan`);
+// ES6: Enhanced Object Property
+// 1. Property Shorthand
+// 2. Method Property
+
+function person(name, age) {
+  return {
+    name,
+    age,
+    getName() {
+      return this.name + " " + this.age;
+    },
+  };
+}
+console.log(person("Quan", 100));
 
 // ES5
-var firstName = 'Quan';
-var fullName = 'Toi ten la ' + firstName + "\n" + "Nam nay toi 23 tuoi";
-console.log(fullName);
+function person1(name, age) {
+  return {
+    name: name,
+    age: age,
+    getName: function () {
+      return this.name + " " + this.age;
+    },
+  };
+}
+console.log(person1("An", 120));
+
+// 3. Computed Property
+var salary = "Salary";
+
+// ES6
+var person2 = {
+  name: "An",
+  age: 50,
+  [`${salary}OfAn`]: 6000,
+}
+console.log(person2);
+
+
+// ES5
+var person3 = {
+  name: "Quan",
+  age: 100,
+};
+person3[salary + "OfQuan"] = 10000000000;
+console.log(person3);
