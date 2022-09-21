bc386d824a6cec04d126f9b67b5f3733608fc58b
Quan Nguyen <nquan1007@gmail.com>
2022-09-21 16:00:47 +0700

    #2 Validate Form: An examples on the Internet with good styles and JS code

--- JavaScript-Exercises/index.js
+++ JavaScript-Exercises/index.js
@@ -1,72 +1,76 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const rePassword = document.getElementById("re-password");
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  validateInputs();
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
 });
 
-const showError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-  errorDisplay.innerText = message;
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
 
-  inputControl.classList.add("invalid");
-  inputControl.classList.remove("success");
-};
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
 
-const showSuccess = (element) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-  errorDisplay.innerText = "";
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
 
-  inputControl.classList.remove("invalid");
-  inputControl.classList.add("success");
-};
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
 
-const isValidEmail = (email) => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-};
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
 
-const validateInputs = () => {
-  const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-  const rePasswordValue = rePassword.value.trim();
-
-  if (usernameValue === "") {
-    showError(username, "Your Full Name is required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (emailValue === "") {
-    showError(email, "Email is required");
-  } else if (!isValidEmail(emailValue)) {
-    showError(email, "Provide a valid email address");
-  } else {
-    showSuccess(email);
-  }
-
-  if (passwordValue === "") {
-    showError(password, "Password is required");
-  } else if (passwordValue.length < 8) {
-    showError(password, "Password must be at least 8 character.");
-  } else {
-    showSuccess(password);
-  }
-
-  if (rePasswordValue === "") {
-    showError(rePassword, "Please confirm your password");
-  } else if (rePasswordValue !== passwordValue) {
-    showError(rePassword, "Passwords doesn't match");
-  } else {
-    showSuccess(rePassword);
-  }
-};
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
